8252f81633b77bf9680cefef9e7693ea10c7c1fc
Julien Cuvillier <julien.cuvillier@mycs.com>
2021-10-22 17:07:46 +0200

    wip

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ async function run() {
     const repo = github.context.repo
     const files = new Map();
     let commitIDs = [];
-    console.log(JSON.stringify(github.context, undefined, 2))
-    for (const c of github.context.payload.commits) {
-      commitIDs.push(c.id)
+    switch(github.context.eventName) {
+      case 'push':
+        commitIDs = getCommitsFromPush();
+        break;
+      case 'pull_request':
+        commitIDs = await getCommitsFromPullRequest(octokit);
+        break;
+      default:
+        core.debug("Unhandled event " + github.context.eventName);
+        return;
+    }
+    for (const id of commitIDs) {
       const {data: commit}  = await octokit.rest.repos.getCommit({ 
         owner: repo.owner,
         repo: repo.repo,
-        ref:  c.id
+        ref:  id
       });
       for (const file of commit.files) {
         add(files, 'all', file.filename)
@@ -33,6 +42,24 @@ async function run() {
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function getCommitsFromPush() {
+  let commitIDs = [];
+  for (const c of github.context.payload.commits) {
+    commitIDs.push(c.id);
+  }
+  return commitIDs;
+}
+
+async function getCommitsFromPullRequest(octokit) {
+  const {data: commits} = await octokit.rest.pulls.listCommits({ 
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.payload.number
+  })
+  console.log(JSON.stringify(commits, undefined, 2));
+  return commits.map(c => c.sha)
 }
 
 function add(map, key, value) {
